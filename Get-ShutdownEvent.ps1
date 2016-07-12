40101c1361cca354a020f9091e90e0988366e83f
Michael Ansbro <mcbro@hotmail.com>
2016-07-12 17:02:38 +0800

    Add files via upload

---
+++ Get-ShutdownEvent.ps1
@@ -0,0 +1,28 @@
+﻿function Get-ShutdownEvent {
+<#
+.SYNOPSIS
+Gets the Windows events which show the last shutdown.
+.DESCRIPTION
+
+.PARAMETER Computername
+Specifys which computer to get the Windows events from.
+
+.EXAMPLE
+ Get-ShutdownEvent -Computername ADServer01
+ 
+#>
+    [CmdletBinding()]
+    param(
+    [Parameter(ValuefromPipeline=$True,
+               HelpMessage="Computer name or IP address" )]
+    [Alias('Hostname', 'Server')]
+    [String]$Computername='localhost'
+    )
+    
+    BEGIN {}
+
+    PROCESS {
+        Get-WinEvent -FilterHashtable @{ Logname='System'; ID='1074'} -ComputerName $Computername
+    }
+    END {}
+}
