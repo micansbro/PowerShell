cdf3ec9e3b1822a364ffc615b6c0c3b8e51065ac
Michael Ansbro <mransbro@gmail.com>
2016-11-17 11:16:06 +0800

    First edit

---
+++ Get-Printer.ps1
@@ -0,0 +1,40 @@
+﻿<#
+.SYNOPSIS
+Gets all local and network installed printers.
+.DESCRIPTION
+This function uses WMI to get all local and 
+network installed printers.
+.PARAMETER Computername
+One or more computer hostnames
+.EXAMPLE
+Get-Printer -ComputerName 'Server01', 'Sever02'
+.EXAMPLE
+'Server01', 'Sever02' | Get-Printer
+.NOTES
+Author: Michael Ansbro
+#>
+function Get-Printer {
+
+[Cmdletbinding()]
+param (
+    [Parameter(ValueFromPipeline=$True)]
+    [String[]]$Computername = $env:COMPUTERNAME
+)
+
+PROCESS {
+foreach ($Comp in $ComputerName) {
+    $Printers = Get-WmiObject -Computername $Comp -Class win32_printer -ErrorAction SilentlyContinue
+        Foreach ($Printer in $Printers){
+            $Obj = New-object -TypeName PSCustomObject -Property @{
+                Hostname = $Comp;
+                PrinterName = $Printer.name;
+                PrinterStatus = $Printer.PrinterStatus;
+                ShareName = $Printer.ShareName
+            }
+            Write-Output $Obj
+         }
+        
+    }
+}
+}
+ 
